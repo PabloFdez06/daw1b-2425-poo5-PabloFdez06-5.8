081d831c2463cc7c1755401a66d62493a902cc04
PabloFdez06 <ffpablo@iesrafaelalberti.es.>
2025-02-10 20:10:17 +0100

    Finalizado

--- src/Estudiante.kt
+++ src/Estudiante.kt
@@ -2,8 +2,11 @@ class Estudiante(val curso: String,
                  val calificacionPromedio: Double,
                  nombre: String,
                  edad: Int,
-                 id: String) {
+                 id: String): Persona(nombre, edad, id) {
 
+    fun mostrarRol(){
+        println("ROL: Estudiante")
+    }
 
     fun mostrarCalificacion(calificacionPromedio: Double){
         println(calificacionPromedio)
--- src/Profesor.kt
+++ src/Profesor.kt
@@ -2,8 +2,11 @@ class Profesor(val departamento: String,
                val aniosExperiencia: Int,
                nombre: String,
                edad: Int,
-               id: String) {
+               id: String): Persona(nombre, edad, id) {
 
+    fun mostrarRol(){
+        println("ROL: Profesor")
+    }
 
     fun mostrarExperiencia(aniosExperiencia: Int){
         println(aniosExperiencia)
